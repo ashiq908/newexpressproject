e8bd2a1dcf805a2094c269a93008e7fac4e17003
ashiq908 <ashikashok37@gmail.com>
2024-09-24 19:17:27 +0530

    mvc

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
+
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const employeeRoutes = require('./routes/employeeRoutes')
 
 const app = express()
+app.use(cors())
 
+
+app.use('/employee',employeeRoutes)
+
+mongoose.connect('').then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 app.listen(8000,()=>{
-    console.log('server running successfully')
+    console.log('server running')
 })
---
+++ backend/routes/employeeRoutes.js
@@ -0,0 +1,13 @@
+
+const express = require('express')
+const { getEmployees, getEmployeeBank } = require('../controllers/employeeController')
+const router = express.Router()
+
+
+router.get('/',getEmployees)
+
+router.get('/bank',getEmployeeBank)
+
+
+
+module.exports = router
