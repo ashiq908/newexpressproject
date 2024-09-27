554d97f3a2d6da6239ff44010bc959b5579a5c99
ashiq908 <ashikashok37@gmail.com>
2024-09-27 20:03:49 +0530

    updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,23 +1,17 @@
-const express = require('express')
+
 const mongoose = require('mongoose')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const userRoutes = require('./routes/userRoutes')
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-
-app.use('/api',userRoutes)
-
-
-//mongoDB connection
-mongoose.connect('mongodb://127.0.0.1:27017/mvc').then(() => {
-    console.log('Connected to MongoDB');
-  }).catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
-
-app.listen(8000,()=>{
-    console.log('server running')
+const userSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required :true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique : true
+    },
 })
+
+
+module.exports = mongoose.model('user',userSchema)
